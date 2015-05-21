943dda05c497f5ba806eca14a05749263863150b
Diliara Nasirova <contact@diliaranasirova.com>
2015-05-21 16:44:03 -0700

    render this cat issue

--- practice/4_catclicker_MVO/app.js
+++ practice/4_catclicker_MVO/app.js
@@ -2,22 +2,23 @@ $(function () {
 
 //model
     var model = {
+        currentCat: null,
         cats: [
             {
-                name: "cat 1",
+                name: "Alfa",
                 clickCount: 0,
                 img: "https://placekitten.com/g/200/300",
                 visible: true
             },
             {
-                name: "cat 2",
+                name: "Betta",
                 clickCount: 0,
                 img: "https://placekitten.com/g/200/301",
                 visible: false
 
             },
             {
-                name: "cat 3",
+                name: "Gamma",
                 clickCount: 0,
                 img: "https://placekitten.com/g/201/301",
                 visible: false
@@ -31,33 +32,30 @@ $(function () {
 
         init: function () {
 
+            //set current cat to the first one in the list
+            model.currentCat = model.cats[0];
 
             catListView.init();
             catView.init();
-
-
         },
 
+        getCurrentCat: function () {
+            return model.currentCat;
+        },
 
         getCats: function () {
-            var cats = model.cats.filter(function (cat) {
-                return cat;
-            });
-            return cats;
-
+            return model.cats;
         },
 
-        getVisibleCats: function () {
-            var visibleCats = model.cats.filter(function (cat) {
-                return cat.visible;
-            });
-            return visibleCats;
+        //set the currently-selected cat to the object we passed in
+        setCurrentCat: function (cat) {
+            model.currentCat = cat;
         },
 
-        makeCatVisible: function () {
+        incrementCounter: function () {
+            model.currentCat.clickCount++;
             catView.render();
         }
-
 
 
     }
@@ -66,24 +64,34 @@ $(function () {
 //view for cat-view
     var catView = {
         init: function () {
+
             // grab elements and html for using in the render function
-            this.$catView = $('.cat-view');
+            this.$catView = $('#cat-view');
+            this.$catClickMe = $("#cat-click-me");
+
             this.catTemplate = $('script[data-template="cat-view"]').html();
+
+            //on click, increment the current cat's counter
+            this.$catClickMe.click(function () {
+                controller.incrementCounter();
+            });
+
+            //render
             this.render();
         },
 
         render: function () {
             // Cache vars for use in forEach() callback (performance)
             var $catView = this.$catView,
+                currentCat = controller.getCurrentCat(),
                 catTemplate = this.catTemplate;
 
-            // Clear and render
-            $catView.html('');
-            controller.getVisibleCats().forEach(function (cat) {
-                // Replace template markers with data
-                var thisTemplate = catTemplate.replace(/{{name}}/g, cat.name).replace(/{{img}}/g, cat.img);
-                $catView.append(thisTemplate);
-            });
+            // $catView.html('');
+
+            // Replace template markers with data
+            var thisTemplate = catTemplate.replace(/{{name}}/g, currentCat.name).replace(/{{img}}/g, currentCat.img).replace(/{{clickCount}}/g, currentCat.clickCount);
+            $catView.append(thisTemplate);
+
         }
     };
 
@@ -91,15 +99,9 @@ $(function () {
 //view for cat-list
     var catListView = {
         init: function () {
-
-            var showCatBtn = $('.show-cat');
-            showCatBtn.click(function () {
-                controller.makeCatVisible();
-            });
-
-
             // grab elements and html for using in the render function
-            this.$catControls = $('.cat-list');
+            this.$catControls = $('#cat-list');
+            this.$renderThisCat = $('.render-this-cat');
             this.catControlsTemplate = $('script[data-template="cat-list"]').html();
             this.render();
         },
@@ -107,15 +109,37 @@ $(function () {
         render: function () {
             // Cache vars for use in forEach() callback (performance)
             var $catControls = this.$catControls,
+                $renderThisCat = this.$renderThisCat,
                 catControlsTemplate = this.catControlsTemplate;
 
-            // Clear and render
-            $catControls.html('');
+
+            // render
             controller.getCats().forEach(function (cat) {
                 // Replace template markers with data
                 var thisTemplate = catControlsTemplate.replace(/{{name}}/g, cat.name);
+
+                /*
+                 $renderThisCat.click(function(){
+                 return function(){
+                 controller.setCurrentCat(cat);
+                 catView.render();
+                 }
+                 })(cat);
+                 */
+
+
+
+                $renderThisCat.on('click', function () {
+                    console.log("render this cat");
+                });
+
                 $catControls.append(thisTemplate);
+
+
+
             });
+
+
         }
     };
 
